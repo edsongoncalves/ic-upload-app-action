2275c5455d561105e8c8d028349c47e2fb6b2eed
Edson <edson@edsongoncalves.com.br>
2024-08-05 14:14:28 -0300

    new release 0.1.7

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const core = require('@actions/core');
 const fs = require('fs');
-const axios = require('axios');
 const FormData = require('form-data');
+const url = require('url');
 
 async function run() {
   try {
@@ -16,15 +16,38 @@ async function run() {
     const form = new FormData();
     form.append('filename', fs.createReadStream(filePath));
 
-    const response = await axios.post(projectUrl, form, {
+    const parsedUrl = new URL(projectUrl);
+    const options = {
+      method: 'POST',
       headers: {
         ...form.getHeaders(),
         'X-Integrator-Container': `${key}`
       }
-    });
+    };
 
-    core.setOutput('response', response.data);
-    console.log('File uploaded successfully', response.data);
+    form.submit({ host: parsedUrl.hostname, path: parsedUrl.pathname, protocol: parsedUrl.protocol, headers: options.headers }, (err, res) => {
+      if (err) {
+        core.setFailed(`Action failed with error: ${err.message}`);
+        return;
+      }
+
+      let data = '';
+      res.on('data', (chunk) => {
+        data += chunk;
+      });
+      res.on('end', () => {
+        if (res.statusCode >= 200 && res.statusCode < 300) {
+          core.setOutput('response', data);
+          console.log('File uploaded successfully', data);
+        } else {
+          core.setFailed(`Upload failed with status code: ${res.statusCode}`);
+        }
+      });
+
+      res.on('error', (error) => {
+        core.setFailed(`Action failed with error: ${error.message}`);
+      });
+    });
   } catch (error) {
     core.setFailed(`Action failed with error: ${error.message}`);
   }
